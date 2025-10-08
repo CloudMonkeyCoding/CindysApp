8657b9042cf36f30da4e44115fd4649537fc0889
EJ Quance <69334632+CloudMonkeyCoding@users.noreply.github.com>
2025-10-08 12:56:12 +0800

    Fix MainActivity resource import

--- app/src/main/java/com/example/deliveryapp/MainActivity.java
+++ app/src/main/java/com/example/deliveryapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.deliveryapp.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
